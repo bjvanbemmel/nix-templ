e12db845ea8632a4192170b57d484f4d33f619b8
bjvanbemmel <beauvanbemmel@live.nl>
2025-01-22 13:06:02 +0100

    remove inputs object from default

--- default/flake.nix
+++ default/flake.nix
@@ -1,10 +1,9 @@
 {
   description = "Default flake template";
 
-  inputs = {
-    nixpkgs.url = "nixpkgs/nixos-24.11";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "nixpkgs/nixos-24.11";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system:
